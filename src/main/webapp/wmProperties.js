cfaad312f744b62330c8d59ebc0b622ac4b5b482
Vineela Maddala <vineela.maddala@wavemaker.com>
2017-09-08 15:34:14 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestProject",
+  "homePage" : "Main",
+  "name" : "TestProject",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
